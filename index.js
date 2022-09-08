c392972c1bbb8dcf71e7f61d573e5042f67444f4
Marcelo Folloni <marfolloni@gmail.com>
2022-09-08 10:30:49 -0300

    aplicando variables

---
+++ index.js
@@ -0,0 +1,20 @@
+/* /* /*Declaracion de una variable*/
+/* let edad;
+ */
+/* asignacion de variable */
+/* edad = 23;
+console.log(edad);
+ */
+
+        /* let nombre = prompt("ingresa tu nombre");
+        let edad = parseInt(prompt("ingresa su edad")); */
+
+        /* let entrada = prompt('Ingrese una letra');
+        let salida = entrada + " " + "ingresada";
+        alert(salida);
+        console.log(); */
+
+let nombre = prompt("Ingresa tu nombre");
+let edad = parseInt(prompt("Ingresa su edad"));
+let saludo = `Hola ${nombre}, tienes ${edad} años`;
+alert(saludo);
